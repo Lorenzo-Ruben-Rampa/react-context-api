5d182a200fe3cb3ebccfc24b78f5f08d8642a545
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-02-26 19:59:32 +0100

    creazione postpage e postlist components

---
+++ src/components/PostsList.jsx
@@ -0,0 +1,8 @@
+export default function PostsList() {
+    return (
+        <>
+            {/* Lista dei post */}
+        </>
+    )
+}
+
---
+++ src/components/PostsPage.jsx
@@ -0,0 +1,11 @@
+import PostsList from "./PostsList"
+
+export default function PostsPage() {
+    return (
+        <>
+            <h1>Pagina del post</h1>
+            < PostsList />
+        </>
+    )
+}
+
