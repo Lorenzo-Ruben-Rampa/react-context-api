182336c5cfe1c279feff69a0d374e1c78f3f15fa
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-02-27 09:04:37 +0100

    correzione postscard

--- src/components/PostsCard.jsx
+++ src/components/PostsCard.jsx
@@ -1,22 +1,22 @@
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-export default function PostCard({ menu }) {
+const PostsCard = (props) => {
+
+    // destructuring dell'oggetto props
+    const { post } = props;
+
     return (
         <>
-            {
-                menu.map((post) => (
-                    <div className='postItem'>
-                        <h2>{post.title}</h2>
-                        <img src={post.image} alt={post.title} />
-                        <p>{post.tags.join(", ")}</p>
-                        <Link to={`/posts/${post.id}`}>Vai al dettaglio</Link>
-                    </div>
+            <div className='postItem'>
+                <h2>{post.name}</h2>
+                <img src={post.image} alt={post.name} />
+                <p>{post.ingredients.join(", ")}</p>
+                <Link to={`/posts/${post.id}`}>Vai al dettaglio</Link>
+            </div>
 
         </>
     )
+
 }
 
-// const PizzasCard = (props) => {
-
-//     // destructuring dell'oggetto props
-//     const { pizza } = props;
+export default PostsCard
