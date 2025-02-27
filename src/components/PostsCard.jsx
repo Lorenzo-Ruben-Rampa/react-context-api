629097d11272888440353b3ffcb05c3d03c0d898
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-02-27 09:02:35 +0100

    correzione PostsList

--- src/components/PostsCard.jsx
+++ src/components/PostsCard.jsx
@@ -1,14 +1,16 @@
 // import { Link } from "react-router-dom";
 
-export default function PostCard() {
+export default function PostCard({ menu }) {
     return (
         <>
-            <div className='postItem'>
-                <h2>{post.title}</h2>
-                <img src={post.image} alt={post.title} />
-                <p>{post.tags.join(", ")}</p>
-                <Link to={`/posts/${post.id}`}>Vai al dettaglio</Link>
-            </div>
+            {
+                menu.map((post) => (
+                    <div className='postItem'>
+                        <h2>{post.title}</h2>
+                        <img src={post.image} alt={post.title} />
+                        <p>{post.tags.join(", ")}</p>
+                        <Link to={`/posts/${post.id}`}>Vai al dettaglio</Link>
+                    </div>
 
         </>
     )
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -4,24 +4,23 @@ import GlobalContext from "../contexts/GlobalContext"
 import PostsCard from "./PostsCard";
 
 const PostsList = () => {
+
     // destrutturiamo l'esecuzione del useContext (oggetto di ritorno)
     const { menu } = useContext(GlobalContext);
 
     return (
         <>
-            < PostsCard />
-            {/* {
-                menu.map((Post) => (
-                    <div className='PostItem' key={Post.id}>
-                        <h2>{Post.title}</h2>
-                        <img className="centered mini" src={Post.image} alt={Post.title} />
-                        <p className="centered">{Post.tags.join(", ")}</p>
-                        <Link to={`/post/${Post.id}`} className="centered">Vai al dettaglio</Link>
-                    </div>
-                )) */}
 
+            {
+                menu.map((post) => (
+                    <PostsCard key={post.id} post={post} />
+                ))
+
+
+            }
         </>
     )
+
 }
 
 export default PostsList
